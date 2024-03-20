0fabdc747f4a4c506ffb1ff4ea5a27af4363cef0
MariaKulo <mariavkulo@gmail.com>
2024-03-20 11:06:11 +0200

    create gitignore

---
+++ src/components/Content.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Content = ({ course }) => {
+    return (
+        <div>
+            {course.parts.map(part =>
+                <p key={part.id}>{part.name} {part.exercises}</p>
+            )}
+        </div>
+    )
+}
+
+export default Content
