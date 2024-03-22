a44109efda12506d77283ad6658b2d285f8ddbe9
MariaKulo <mariavkulo@gmail.com>
2024-03-22 21:05:34 +0200

    Tehtävä 2.2.

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 const Content = ({ course }) => {
+
+    const totalExercises = course.parts.reduce((total, part) => total + part.exercises, 0)
+
     return (
         <div>
-            {course.parts.map(part =>
+            {course.parts.map(part => (
                 <p key={part.id}>{part.name} {part.exercises}</p>
-            )}
+            ))}
+                <p><strong>Total of {totalExercises} exercises</strong></p>
         </div>
     )
 }
